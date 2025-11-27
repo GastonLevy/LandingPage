6ce62587821bb8039c910081b6b6ae40b9a5087b
Dusan Nika <dusannika@gmail.com>
2025-11-27 12:15:27 +0100

    navbar,hero,tech,teacher

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NavbarComponent } from './components/navbar/navbar';
+import { HeroComponent } from './components/hero/hero';
+import { TeacherComponent } from './components/teacher/teacher';
+import { TechComponent } from './components/tech/tech';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, TeacherComponent, TechComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('app');
---
+++ src/app/components/teacher/teacher.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher',
+  standalone: true,
+  templateUrl: './teacher.html',
+  styleUrl: './teacher.css',
+})
+export class TeacherComponent {}
