e9642925e861e17f3d24481a92a3c4cec361c02e
Gaston <ggastlino@gmail.com>
2025-11-27 18:00:48 +0100

    Router outlet implemented and router links updated.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-//import { HomePageComponent } from './pages/home-page/home-page.component';
-//import { AboutUsPageComponent } from './pages/about-us-page/about-us-page.component';
-//import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { Routes } from '@angular/router';
 
-const routes: Routes = [
-  //{ path: '', component: HomePageComponent }, // main page
-  //{ path: 'about-us', component: AboutUsPageComponent }, // About us page
-  //{ path: 'contact', component: ContactPageComponent }, // Contact page
-  { path: '**', redirectTo: '' }, // fallback -> main page
+export const routes: Routes = [
+  {
+    path: 'about-us',
+    loadComponent: () => import('./pages/about-us/about-us').then((m) => m.AboutUs),
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact/contact').then((m) => m.Contact),
+  },
+  { path: '**', redirectTo: 'about-us' },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
