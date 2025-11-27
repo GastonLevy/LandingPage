e87b88fb64528eedebae3275289c164c901e34d2
Gaston <ggastlino@gmail.com>
2025-11-27 16:42:22 +0100

    Future routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+//import { HomePageComponent } from './pages/home-page/home-page.component';
+//import { AboutUsPageComponent } from './pages/about-us-page/about-us-page.component';
+//import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  //{ path: '', component: HomePageComponent }, // main page
+  //{ path: 'about-us', component: AboutUsPageComponent }, // About us page
+  //{ path: 'contact', component: ContactPageComponent }, // Contact page
+  { path: '**', redirectTo: '' }, // fallback -> main page
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
