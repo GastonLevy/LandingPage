ed4c03b68419ed7732bd8c565d668d00d6b966b6
Gaston <ggastlino@gmail.com>
2025-11-30 19:11:13 +0100

    Route to home page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () => import('./pages/home/home').then((m) => m.Home),
+  },
+  {
     path: 'about-us',
     loadComponent: () => import('./pages/about-us/about-us').then((m) => m.AboutUs),
   },
@@ -9,5 +13,8 @@ export const routes: Routes = [
     path: 'contact',
     loadComponent: () => import('./pages/contact/contact').then((m) => m.Contact),
   },
-  { path: '**', redirectTo: 'about-us' },
+  {
+    path: '**',
+    redirectTo: '', // antes estaba 'about-us'
+  },
 ];
