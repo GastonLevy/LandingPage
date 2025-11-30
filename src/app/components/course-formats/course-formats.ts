8783f3d647ac448019f9d9570844d4037447d7f6
Patricia Ferkova <pat.ferkova@gmail.com>
2025-11-30 15:34:28 +0100

    Course Formats

---
+++ src/app/components/course-formats/course-formats.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-formats',
+  imports: [],
+  templateUrl: './course-formats.html',
+  styleUrl: './course-formats.css',
+})
+export class CourseFormats {
+
+}
