29759cc936752cc9408fefded3623be454b1a546
Gaston <ggastlino@gmail.com>
2025-12-01 00:41:43 +0100

    Json for course formats implemented.

---
+++ src/app/pages/home/course-formats/course-formats.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { CourseFormatsConfig } from './course-formats.types';
+
+@Component({
+  selector: 'app-course-formats',
+  standalone: true,
+  templateUrl: './course-formats.html',
+  styleUrl: './course-formats.css',
+})
+export class CourseFormats {
+  config: CourseFormatsConfig = {
+    heading: 'Course Types',
+    subheading: 'Choose type of course which fits you the best!',
+
+    formats: [
+      {
+        title: 'Online',
+        features: ['Online access to all courses', 'Online classes', 'Online teacher support'],
+        buttonText: 'More information',
+        buttonClass: 'w-100 btn btn-lg btn-outline-dark',
+      },
+      {
+        title: 'Present',
+        features: [
+          'Online access to all courses',
+          'Present classes at school',
+          'Support from teachers at school',
+        ],
+        buttonText: 'More information',
+        buttonClass: 'w-100 btn btn-lg btn-primary',
+      },
+      {
+        title: 'Combined',
+        features: [
+          'Online access to all courses',
+          'Switching between Online and Present classes',
+          'Support from teachers online or at school',
+        ],
+        buttonText: 'More information',
+        buttonClass: 'w-100 btn btn-lg btn-primary',
+        cardClass: 'border-primary',
+        headerClass: 'text-bg-primary border-primary',
+      },
+    ],
+  };
+}
