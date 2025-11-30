d3cdb29fc2758daf0aedf429282ed40e7b362013
Gaston <ggastlino@gmail.com>
2025-11-30 19:11:42 +0100

    Tschüss, Baba!

--- src/app/components/teacher/teacher.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-teacher',
-  standalone: true,
-  templateUrl: './teacher.html',
-  styleUrl: './teacher.css',
-})
-export class TeacherComponent {}
