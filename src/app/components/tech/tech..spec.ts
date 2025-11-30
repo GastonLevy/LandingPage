8a3c62818f5f98677fca7a72db97b71c1604284b
Gaston <ggastlino@gmail.com>
2025-11-30 19:12:38 +0100

    Tschüss, Baba!

--- src/app/components/tech/tech..spec.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-tech',
-  standalone: true,
-  templateUrl: './tech.html',
-  styleUrl: './tech.css',
-})
-export class TechComponent {}
